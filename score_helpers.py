8d8c16aec20bfbf6a8549d4cfde212d9993620f9
Sarin, Vishesh <vs62@hw.ac.uk>
2021-04-27 17:44:14 +0530

    Moved score based display of randomized serach model metrics to a separate file

---
+++ score_helpers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def display_scores(scores):
+    print("Scores: {0}\nMean: {1:.3f}\nStd: {2:.3f}".format(scores, np.mean(scores), np.std(scores)))
+    
+def report_best_scores(results, n_top=3):
+    for i in range(1, n_top + 1):
+#         i = 201 - i
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                  results['mean_test_score'][candidate],
+                  results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
